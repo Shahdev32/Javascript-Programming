51eebd2062fad8d1647e24a443c5d36623472644
shahdev kumar <shahdevkumar.d@gmail.com>
2024-02-29 00:01:01 +0500

    all new code of js

--- myscripts.js
+++ myscripts.js
@@ -38,3 +38,14 @@ const person = {
       return this.firstName + " " + this.lastName;
     }
   };
+
+
+  // math function 
+  Math.E        // returns Euler's number
+Math.PI       // returns PI
+Math.SQRT2    // returns the square root of 2
+Math.SQRT1_2  // returns the square root of 1/2
+Math.LN2      // returns the natural logarithm of 2
+Math.LN10     // returns the natural logarithm of 10
+Math.LOG2E    // returns base 2 logarithm of E
+Math.LOG10E
