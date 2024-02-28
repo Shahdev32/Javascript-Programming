d79e6e92a5e5e31d51edf8f7762637674f865f9a
shahdev kumar <shahdevkumar.d@gmail.com>
2024-02-28 23:53:18 +0500

    add other files

--- myscripts.js
+++ myscripts.js
@@ -1,17 +1,17 @@
-console.log("Hello ");
-console.log("Shahdev Kumar");
-let a =10;
-let b=5;
-let sum = a+b;
-console.log(sum);
+// console.log("Hello ");
+// console.log("Shahdev Kumar");
+// let a =10;
+// let b=5;
+// let sum = a+b;
+// console.log(sum);
 
-//template literals
+// //template literals
 
-let pensilePrice =10;
-let erasePrice =15;
-//console.log("the total price is :"+(pensilePrice + erasePrice)+"Rupees".)
-let output =  `the total Price is :${pensilePrice + erasePrice} Rupees `;
-console.log(output);
+// let pensilePrice =10;
+// let erasePrice =15;
+// //console.log("the total price is :"+(pensilePrice + erasePrice)+"Rupees".)
+// let output =  `the total Price is :${pensilePrice + erasePrice} Rupees `;
+// console.log(output);
 
 
 //operators
@@ -26,3 +26,15 @@ console.log(a**b);
 console.log(a++);
 console.log(+aa);
 
+
+
+
+//object literals
+const person = {
+    firstName: "John",
+    lastName : "Doe",
+    id       : 5566,
+    fullName : function() {
+      return this.firstName + " " + this.lastName;
+    }
+  };
