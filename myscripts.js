8783e85f131dc6e30637784ac40e5f436c6ff994
shahdev kumar <shahdevkumar.d@gmail.com>
2024-02-28 21:29:07 +0500

    all new code of js

---
+++ myscripts.js
@@ -0,0 +1,28 @@
+console.log("Hello ");
+console.log("Shahdev Kumar");
+let a =10;
+let b=5;
+let sum = a+b;
+console.log(sum);
+
+//template literals
+
+let pensilePrice =10;
+let erasePrice =15;
+//console.log("the total price is :"+(pensilePrice + erasePrice)+"Rupees".)
+let output =  `the total Price is :${pensilePrice + erasePrice} Rupees `;
+console.log(output);
+
+
+//operators
+let a=5;
+let b=2;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+console.log(a++);
+console.log(+aa);
+
