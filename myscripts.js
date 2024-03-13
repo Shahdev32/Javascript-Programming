921777fa179c6c65a0cd5da03e617ae93abca021
shahdev kumar <shahdevkumar.d@gmail.com>
2024-03-13 23:01:03 +0500

    comment

--- myscripts.js
+++ myscripts.js
@@ -1,160 +1,172 @@
-// console.log("Hello ");
-// console.log("Shahdev Kumar");
-// let a =10;
-// let b=5;
-// let sum = a+b;
-// console.log(sum);
+// // console.log("Hello ");
+// // console.log("Shahdev Kumar");
+// // let a =10;
+// // let b=5;
+// // let sum = a+b;
+// // console.log(sum);
 
-// //template literals
+// // //template literals
 
-// let pensilePrice =10;
-// let erasePrice =15;
-// //console.log("the total price is :"+(pensilePrice + erasePrice)+"Rupees".)
-// let output =  `the total Price is :${pensilePrice + erasePrice} Rupees `;
-// console.log(output);
-
-
-//operators
-/*let a=5;
-let b=2;
-console.log(a+b);
-console.log(a-b);
-console.log(a*b);
-console.log(a/b);
-console.log(a%b);
-console.log(a**b);
-console.log(a++);
-console.log(+aa);
+// // let pensilePrice =10;
+// // let erasePrice =15;
+// // //console.log("the total price is :"+(pensilePrice + erasePrice)+"Rupees".)
+// // let output =  `the total Price is :${pensilePrice + erasePrice} Rupees `;
+// // console.log(output);
 
 
-
-
-//object literals
-const person = {
-    firstName: "John",
-    lastName : "Doe",
-    id       : 5566,
-    fullName : function() {
-      return this.firstName + " " + this.lastName;
-    }
-  };
-
-
-  // math function 
-  Math.E        // returns Euler's number
-Math.PI       // returns PI
-Math.SQRT2    // returns the square root of 2
-Math.SQRT1_2  // returns the square root of 1/2
-Math.LN2      // returns the natural logarithm of 2
-Math.LN10     // returns the natural logarithm of 10
-Math.LOG2E    // returns base 2 logarithm of E
-Math.LOG10E
-
-
-//
-
-function hello()
-{
-    console.log("Hello")
-
-}
-
-
-hello()
-
-// function with arguments 
-function info(name,age){
-    console.log( `the ${name}'s age is ${age}`)
-
-}
-
-info();
-
-
-///
-function sum(a,b){
-    return a+b;/
-}
-
-console.log(sum(3,4));
+// //operators
+// /*let a=5;
+// let b=2;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+// console.log(a++);
+// console.log(+aa);
 
 
 
 
-// arrow function 
-
-const sum  = (a,b) => {
-    console.log(a+b)
-}
-
-// calling
-sum(3,4);
-
-const cube = (n) => {
-    return n*n*n;
-
-}
+// //object literals
+// const person = {
+//     firstName: "John",
+//     lastName : "Doe",
+//     id       : 5566,
+//     fullName : function() {
+//       return this.firstName + " " + this.lastName;
+//     }
+//   };
 
 
-cube(2);
+//   // math function 
+//   Math.E        // returns Euler's number
+// Math.PI       // returns PI
+// Math.SQRT2    // returns the square root of 2
+// Math.SQRT1_2  // returns the square root of 1/2
+// Math.LN2      // returns the natural logarithm of 2
+// Math.LN10     // returns the natural logarithm of 10
+// Math.LOG2E    // returns base 2 logarithm of E
+// Math.LOG10E
 
 
-// implict arrow function
-const mul = (a,b)  => {
-    a*b;
+// //
+
+// function hello()
+// {
+//     console.log("Hello")
+
+// }
 
 
-}
+// hello()
+
+// // function with arguments 
+// function info(name,age){
+//     console.log( `the ${name}'s age is ${age}`)
+
+// }
+
+// info();
+
+
+// ///
+// function sum(a,b){
+//     return a+b;/
+// }
+
+// console.log(sum(3,4));
 
 
 
-//DOM
-//doucment oject model
 
-  */
+// // arrow function 
+
+// const sum  = (a,b) => {
+//     console.log(a+b)
+// }
+
+// // calling
+// sum(3,4);
+
+// const cube = (n) => {
+//     return n*n*n;
+
+// }
+
+
+// cube(2);
+
+
+// // implict arrow function
+// const mul = (a,b)  => {
+//     a*b;
+
+
+// }
+
+
+
+// //DOM
+// //doucment oject model
+
+//   */
  
 
-// this is events 
-/*
+// // this is events 
+// /*
 
-<button onclick="document.getElementById('demo').innerHTML = Date()">The time is?</button>
-<button onclick="this.innerHTML = Date()">The time is?</button>
-<button onclick="displayDate()">The time is?</button>
+// <button onclick="document.getElementById('demo').innerHTML = Date()">The time is?</button>
+// <button onclick="this.innerHTML = Date()">The time is?</button>
+// <button onclick="displayDate()">The time is?</button>
 
-*/
+// */
 
-// Add event listener to the button
-let btn = document.querySelector("button");
-btn.addEventListener("click", function () {
-    // Get reference to the h3 element
-    let h3 = document.querySelector("h3");
+// /*
+// // Add event listener to the button
+// let btn = document.querySelector("button");
+// btn.addEventListener("click", function () {
+//     // Get reference to the h3 element
+//     let h3 = document.querySelector("h3");
     
-    // Generate a random color
-    let randomColor = getRandomColor();
+//     // Generate a random color
+//     let randomColor = getRandomColor();
     
-    // Update the text content of the h3 element with the generated color
-    h3.innerText = randomColor;
+//     // Update the text content of the h3 element with the generated color
+//     h3.innerText = randomColor;
 
-    // Get reference to the div element
-    let div = document.querySelector("div");
-    
-    // Change the background color of the div element to the generated color
-    div.style.backgroundColor = randomColor;
-    
-    // Log a message to indicate that the color has been updated
-    console.log("Color updated");
-});
 
-// Function to generate a random color
-function getRandomColor() {
-    // Generate random values for red, green, and blue components
-    let red = Math.floor(Math.random() * 256); // Use Math.random() * 256 to include 255
-    let green = Math.floor(Math.random() * 256);
-    let blue = Math.floor(Math.random() * 256);
+
+
+//     // Get reference to the div element
+//     let div = document.querySelector("div");
     
-    // Construct the RGBA color string
-    let color = `rgba(${red},${green},${blue},1)`; // Set alpha to 1 for fully opaque color
+//     // Change the background color of the div element to the generated color
+//     div.style.backgroundColor = randomColor;
     
-    // Return the generated color
-    return color;
-}
+//     // Log a message to indicate that the color has been updated
+//     console.log("Color updated");
+// });
+
+
+  
+// // Function to generate a random color
+// function getRandomColor() {
+//     // Generate random values for red, green, and blue components
+//     let red = Math.floor(Math.random() * 256); // Use Math.random() * 256 to include 255
+//     let green = Math.floor(Math.random() * 256);
+//     let blue = Math.floor(Math.random() * 256);
+    
+//     // Construct the RGBA color string
+//     let color = `rgba(${red},${green},${blue},1)`; // Set alpha to 1 for fully opaque color
+    
+//     // Return the generated color
+//      return color;
+// }
+
+
+
+
+// */
+
